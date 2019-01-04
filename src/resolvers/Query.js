dcd2237390c549ae822ee1fd031f430e98d776a6
Recox <5874806+RecoX@users.noreply.github.com>
2019-01-04 23:47:07 +1300

    Added get image by id

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,102 +1,111 @@
 async function feed(parent, args, context) {
 
-  const count = await context.prisma
-    .linksConnection({
-      where: {
-        OR: [
-          { description_contains: args.filter },
-          { url_contains: args.filter },
-        ],
-      },
-      skip: args.skip,
-    })
-    .aggregate()
-    .count();
+    const count = await context.prisma
+        .linksConnection({
+            where: {
+                OR: [
+                    { description_contains: args.filter },
+                    { url_contains: args.filter },
+                ],
+            },
+            skip: args.skip,
+        })
+        .aggregate()
+        .count();
 
-  const links = await context.prisma.links({
-    where: {
-      OR: [
-        { description_contains: args.filter },
-        { url_contains: args.filter },
-      ],
-    },
-    skip: args.skip,
-    first: args.first,
-    orderBy: args.orderBy,
-  });
+    const links = await context.prisma.links({
+        where: {
+            OR: [
+                { description_contains: args.filter },
+                { url_contains: args.filter },
+            ],
+        },
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy,
+    });
 
-  return {
-    count,
-    links,
-  }
+    return {
+        count,
+        links,
+    }
 }
 
 async function imageList(parent, args, context) {
 
-  const count = await context.prisma
-      .imagesConnection({
+    const count = await context.prisma
+        .imagesConnection({
+            where: {
+                OR: [
+                    { name_contains: args.filter },
+                    { description_contains: args.filter },
+                    { origin_contains: args.filter },
+                ],
+            },
+            skip: args.skip,
+        })
+        .aggregate()
+        .count();
+
+    const images = await context.prisma.images({
         where: {
-          OR: [
-            { name_contains: args.filter },
-            { description_contains: args.filter },
-            { origin_contains: args.filter },
-          ],
+            OR: [
+                { name_contains: args.filter },
+                { description_contains: args.filter },
+                { origin_contains: args.filter },
+            ],
         },
         skip: args.skip,
-      })
-      .aggregate()
-      .count();
+        first: args.first,
+        orderBy: args.orderBy,
+    });
 
-  const images = await context.prisma.images({
-    where: {
-      OR: [
-        { name_contains: args.filter },
-        { description_contains: args.filter },
-        { origin_contains: args.filter },
-      ],
-    },
-    skip: args.skip,
-    first: args.first,
-    orderBy: args.orderBy,
-  });
-
-  return {
-    count,
-    images,
-  }
+    return {
+        count,
+        images,
+    }
 }
 
 
 async function users(parent, args, context) {
 
-  const users = await context.prisma.users({
-    where: {
-      OR: [
-        { name_contains: args.name },
-      ],
-    },
-    skip: args.skip,
-    first: args.first,
-    orderBy: args.orderBy,
-  });
+    const users = await context.prisma.users({
+        where: {
+            OR: [
+                { name_contains: args.name },
+            ],
+        },
+        skip: args.skip,
+        first: args.first,
+        orderBy: args.orderBy,
+    });
 
-  return users
+    return users
 
 }
 
 async function user(parent, args, context) {
 
-  const user = await context.prisma.user({
-    id: args.id
-  });
+    const user = await context.prisma.user({
+        id: args.id
+    });
 
-  return user
+    return user
+}
+
+async function image(parent, args, context) {
+    const image = await context.prisma.image({
+        id: args.id
+    })
+
+    return image
 }
 
 
 module.exports = {
-  feed,
-  imageList,
-  users,
-  user,
+    feed,
+    imageList,
+    image,
+    users,
+    user,
 }
